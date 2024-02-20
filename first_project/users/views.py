ee73d50c63674a976cead162fdb107adcf9f1ea2
AlexArciszewski <arciszew@yahoo.com>
2024-02-20 13:09:24 +0100

    Added login and logout. Loggout panel needs bug fix

--- first_project/users/views.py
+++ first_project/users/views.py
@@ -9,9 +9,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Account's been created for {username}!")
-            return redirect('blog-home')
+            messages.success(request, f"Dear{username}, you have been signed up!")
+            return redirect('login')
     else:
         form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+def profile(request):
+    return render(request, 'users/profile.html')
